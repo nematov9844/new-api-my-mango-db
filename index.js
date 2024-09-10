2551d13ed49862817f79e144598e273ad977c8c5
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-09-10 17:44:24 +0500

    main

--- index.js
+++ index.js
@@ -3,24 +3,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
-
-// Foydalanuvchilarning IP-manzilini cheklash
-const allowedIP = '89.236.218.41'; // Ruxsat berilgan IP-manzil
-
-// IP manzilni tekshiruvchi middleware
-const checkIP = (req, res, next) => {
-    const clientIP = req.ip.replace('::ffff:', ''); // IPv4 manzilini olish
-    if (clientIP === allowedIP) {
-        next(); // Ruxsat berilgan IP bo'lsa, keyingi bosqichga o'tadi
-    } else {
-        res.status(403).json({ message: 'Bu APIga kirishga ruxsat berilmagan!' }); // Ruxsat yo'q
-    }
-};
-
-// End-pointlar uchun middleware qo'llash
-app.use(checkIP);
 
 // Foydalanuvchilarni saqlaydigan array
 let users = [];
